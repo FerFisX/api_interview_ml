e7e9201a3097a9d695d05f4850c0a3671462a928
Fernando Acarapi <130022317+FerFisX@users.noreply.github.com>
2025-05-19 05:49:16 -0400

    New funcionts

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
-import joblib
 import pandas as pd
-
-# Cargar el modelo entrenado
-try:
-    model = joblib.load("models/ml_pipeline.joblib")
-except FileNotFoundError:
-    raise Exception("El archivo del modelo no se encontró. Asegúrate de que 'models/ml_pipeline.joblib' exista.")
+import joblib
 
 app = FastAPI()
-
+app.mount("/static", StaticFiles(directory="static"), name="static")
 class PredictionInput(BaseModel):
     temporada: int
     anio: int
@@ -28,19 +23,24 @@ class PredictionInput(BaseModel):
 class PredictionOutput(BaseModel):
     total_alquileres_predicho: float
 
-@app.post("/predict")
-async def predict_rentals(data: PredictionInput):
-    try:
-        # Convertir los datos de entrada a un DataFrame de pandas
-        input_df = pd.DataFrame([data.dict()])
-
-        # Realizar la predicción utilizando el pipeline cargado
-        prediction = model.predict(input_df)[0]
-
-        return PredictionOutput(total_alquileres_predicho=prediction)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+try:
+    model = joblib.load("models/ml_pipeline.joblib")
+except Exception as e:
+    print(f"Error AL CARGAR el modelo: {e}")
+    # Considerar levantar una excepción aquí para que Render lo registre como un fallo de inicio
 
 @app.get("/")
 async def root():
-    return {"message": "¡La API de predicción de alquileres está funcionando! :D"}
+    return {"message": "¡La API de predicción de alquileres está funcionando!"}
+
+@app.post("/predict")
+async def predict_rentals(data: PredictionInput):
+    print("Entrando a la función predict_rentals")  # Añadimos este print al inicio
+    try:
+        input_data = pd.DataFrame([data.dict()])
+        prediction = model.predict(input_data)[0]
+        return PredictionOutput(total_alquileres_predicho=prediction)
+    except Exception as e:
+        error_message = f"Error DURANTE la predicción: {e}"
+        print(error_message)
+        raise HTTPException(status_code=500, detail=error_message)
